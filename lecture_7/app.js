d0076dca91d7d20d13068650adcf4fb7510cbe1f
Nika munjisvili <nikamunja31415@gmail.com>
2024-02-09 21:17:32 +0400

    add

--- lecture_7/app.js
+++ lecture_7/app.js
@@ -37,26 +37,113 @@
 //   }
 // });
 
-import { Box } from "./styles/box.style.js";
+// import { Box } from "./styles/box.style.js";
 
-Box(
-  "100px",
-  "100px",
-  "red",
-  "20px",
-  "https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png"
-);
-Box(
-  "200px",
-  "200px",
-  "blue",
-  "20px",
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png"
-);
-Box(
-  "300px",
-  "300px",
-  "green",
-  "20px",
-  "https://cdn-icons-png.flaticon.com/512/25/25231.png"
-);
+// Box(
+//   "100px",
+//   "100px",
+//   "red",
+//   "20px",
+//   "https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png"
+// );
+// Box(
+//   "200px",
+//   "200px",
+//   "blue",
+//   "20px",
+//   "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png"
+// );
+// Box(
+//   "300px",
+//   "300px",
+//   "green",
+//   "20px",
+//   "https://cdn-icons-png.flaticon.com/512/25/25231.png"
+// );
+
+// const signin = document.getElementById("signin");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const landing_page = document.querySelector(".second_container");
+// const form = document.querySelector("form");
+
+// signin.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   let user_data = [];
+//   const email_value = email.value;
+//   const password_value = password.value;
+
+//   if (email_value === "" && password_value === "") {
+//     alert("Please Enter something...");
+//   } else {
+//     landing_page.style.display = "block";
+//     form.style.display = "none";
+//     user_data.push(email_value, password_value);
+//     localStorage.setItem("data", JSON.stringify(user_data));
+//     const get_user = localStorage.getItem("data");
+//     const parse_data = JSON.parse(get_user);
+
+//     parse_data.forEach((item) => {
+//       landing_page.innerHTML = `Data: ${item}`;
+//     });
+//   }
+// });
+
+// const numbers = [1, 24, 35, 3, 2, 32, 434, 545, 332, 53, 4];
+
+// const result = numbers.sort();
+// console.log(result);
+
+// function sortMyArray(elements) {
+//   const sordetArray = elements.sort();
+//   return sordetArray;
+// }
+
+// console.log(sortMyArray(numbers));
+
+const numbers = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  [8, 9, [10, 11, [232, 3232, [323, 32333]]]],
+  12,
+  13,
+];
+//result = [1,2,3,4,5,6,7,8,9,10,11]
+
+// function recursiveData(elements) {
+//   let new_array = [];
+
+//   for (const element of elements) {
+//     if (Array.isArray(element)) {
+//       new_array = new_array.concat(recursiveData(element));
+//     } else {
+//       new_array.push(element);
+//     }
+//   }
+//   return new_array;
+// }
+
+// console.log(recursiveData(numbers));
+
+// n = 5;
+
+// 1 * 2 * 3 * 4 * n
+
+function recursiveFunction(n) {
+  if (n === 0) {
+    return 0;
+  }
+  
+  if (n === 1) {
+    return 1;
+  }
+
+  return recursiveFunction(n - 1) * n;
+}
+
+console.log(recursiveFunction(10));
